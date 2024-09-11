b18bf27e9303de38c22a79a7827709663f0ee884
capybara120404 <D.L.Borodin@outlook.com>
2024-09-11 17:23:20 +0300

    Added the ability to set a listening port via configuration files.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load("configs/.env")
+	if err != nil {
+		log.Fatalf("An error occurred while importing configuration files: %v", err)
+		return
+	}
+
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir("web")))
-	err := http.ListenAndServe(":7540" , mux)
+	addr := fmt.Sprintf(":%s", os.Getenv("TODO_PORT"))
+
+	err = http.ListenAndServe(addr, mux)
 	if err != nil {
 		log.Fatalf("The server could not be started due to an error: %v", err)
 		return
