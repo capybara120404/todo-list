7aa659902b5de92c32702086022acaf5ba82259b
capybara120404 <D.L.Borodin@outlook.com>
2024-09-14 21:36:08 +0300

    The structure and functions for connecting to the database have been created.

---
+++ database/database.go
@@ -0,0 +1,60 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Connecter struct {
+	db *sql.DB
+}
+
+func OpenOrCreate() (*Connecter, error) {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		return nil, fmt.Errorf("error getting working directory: %v", err)
+	}
+
+	dbFile := filepath.Join(currentDir, "scheduler.db")
+
+	_, err = os.Stat(dbFile)
+	var create bool
+	if err != nil {
+		create = true
+	}
+
+	db, err := sql.Open("sqlite3", dbFile)
+	if err != nil {
+		return nil, fmt.Errorf("error opening database: %v", err)
+	}
+
+	if create {
+		err = createDatabase(db)
+		if err != nil {
+			return nil, fmt.Errorf("error creating database: %v", err)
+		}
+	}
+
+	return &Connecter{db: db}, nil
+}
+
+func (c *Connecter) Close() {
+	defer c.db.Close()
+}
+
+func createDatabase(db *sql.DB) error {
+	query := `CREATE TABLE scheduler (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		date TEXT NOT NULL,
+		title TEXT NOT NULL,
+		comment TEXT NOT NULL,
+		repeat TEXT NOT NULL
+	);
+	CREATE INDEX date_index ON scheduler(date);`
+	_, err := db.Exec(query)
+	return err
+}
