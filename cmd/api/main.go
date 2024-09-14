f00d3e8bf0a60a5601a9f621b28ff770c4bb04c2
capybara120404 <D.L.Borodin@outlook.com>
2024-09-14 21:36:54 +0300

    Fixed error messages and added a database connection.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,23 +6,31 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/capybara120404/todo-list/database"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	err := godotenv.Load("configs/.env")
 	if err != nil {
-		log.Printf("An error occurred while importing configuration files")
+		log.Printf("an error occurred while importing configuration files: %v", err)
 		return
 	}
 
+	connecter, err := database.OpenOrCreate()
+	if err != nil {
+		log.Printf("error opening or creating database: %v", err)
+		return
+	}
+	defer connecter.Close()
+	
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir("web")))
 	addr := fmt.Sprintf(":%s", os.Getenv("TODO_PORT"))
 
 	err = http.ListenAndServe(addr, mux)
 	if err != nil {
-		log.Printf("The server could not be started due to an error")
+		log.Printf("the server could not be started due to an error: %v", err)
 		return
 	}
 }
