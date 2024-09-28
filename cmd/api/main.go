a44bc82bcfd33396a8a43ea502d3b50d41d0a698
capybara120404 <D.L.Borodin@outlook.com>
2024-09-28 18:09:49 +0300

    The structure and methods of interaction between the layers of the project have been changed.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,37 +4,42 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/capybara120404/todo-list/configs"
-	"github.com/capybara120404/todo-list/handlers"
+	"github.com/capybara120404/todo-list/internal/configs"
+	"github.com/capybara120404/todo-list/internal/database"
+	"github.com/capybara120404/todo-list/internal/handlers"
+	"github.com/capybara120404/todo-list/internal/repository"
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	connecter, err := handlers.OpenOrCreate(configs.PathToDB)
+	connecter, err := database.OpenOrCreate(configs.PathToDB)
 	if err != nil {
-		log.Printf("error opening or creating database: %v", err)
+		log.Printf("%v", err)
 		return
 	}
 	defer connecter.Close()
+
+	repository := repository.NewTaskRepository(connecter)
+	handlers := handlers.NewTaskHandler(repository)
 
 	router := chi.NewRouter()
 
 	fs := http.FileServer(http.Dir("web"))
 
 	router.Handle("/*", http.StripPrefix("/", fs))
-	router.Get("/api/nextdate", connecter.NexDateHandler)
-	router.Get("/api/tasks", connecter.GetTasksHandler)
-	router.Get("/api/task", connecter.GetTaskByIdHandler)
-	router.Post("/api/task", connecter.AddTaskHandler)
-	router.Post("/api/task/done", connecter.MarkAsCompletedHandler)
-	router.Put("/api/task", connecter.ChangeTaskHandler)
-	router.Delete("/api/task", connecter.DeleteTaskHandler)
+	router.Get("/api/nextdate", handlers.NexDateHandler)
+	router.Get("/api/tasks", handlers.GetAllTasksHandler)
+	router.Get("/api/task", handlers.GetTaskByIdHandler)
+	router.Post("/api/task", handlers.AddTaskHandler)
+	router.Post("/api/task/done", handlers.CompleteTaskHandler)
+	router.Put("/api/task", handlers.ChangeTaskHandler)
+	router.Delete("/api/task", handlers.DeleteTaskHandler)
 
-	log.Printf("the server start at port: %s", configs.Addr)
+	log.Printf("The server start at port: %s", configs.Addr)
 
 	err = http.ListenAndServe(configs.Addr, router)
 	if err != nil {
-		log.Printf("the server could not be started due to an error: %v", err)
+		log.Printf("The server could not be started due to an error: %v", err)
 		return
 	}
 }
